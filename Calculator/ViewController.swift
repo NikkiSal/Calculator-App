86b3991154c17c4abff6b79ebf232871772f26f0
Nikki <Nikkisali9@gmail.com>
2020-02-06 18:54:54 -0700

    the numbers and some of the functions work, with the error of decimal place gone

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,21 +11,49 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
-    
+    private var isFinishedTypingNumber = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double.")
+        }
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else {
+                displayLabel.text = String(number/100)
+            }
+        }
     }
-
-    
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." { // this is a very confusing peice of code!
+                    if let currentText = displayLabel.text {
+                        if !currentText.contains(".") {
+                            displayLabel.text = displayLabel.text! + numValue
+                        }
+                        return
+                    }
+//                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+//                        fatalError("Cannot convert display label text to a Double!")
+//                    }
+//                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+//                    if !isInt {
+//                        return
+//                    }
+                }
+                 displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
-
 }
 
